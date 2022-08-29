8305e96879b0a75a4d39294d5d7a735165d8cc75
tarun0206 <tarun.singh.0206@gmail.com>
2022-08-29 20:19:16 +0530

    did the homework

--- class12/class12-materials/class-weekend-boring/js/main.js
+++ class12/class12-materials/class-weekend-boring/js/main.js
@@ -3,7 +3,13 @@ document.querySelector('#check').addEventListener('click', check)
 function check() {
 
   const day = document.querySelector('#day').value
-
+  const main = day.toLowerCase()
+  if(main == 'saturday' || main == 'sunday'){
+    document.querySelector('#placeToSee').innerHTML = 'WEEKENDDDD!!!'
+  }
+  else{
+    document.querySelector('#placeToSee').innerHTML = 'BOOOOOOOOORINGG!!!'
+  }
   //Conditionals go here
 
 
