5bc213b6218ec9e39e37138d596b9334f58aa577
tarun0206 <tarun.singh.0206@gmail.com>
2022-10-13 23:18:08 +0530

    added more homeworks

--- class08/class08-materials/sourceLayout/js/main.js
+++ class08/class08-materials/sourceLayout/js/main.js
@@ -1,1 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.hLinks');
 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+})
